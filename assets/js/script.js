3ba382925f0ec55a83c6cb61bad1b600a5fcfd06
silchenkoofficial <nikita.silchenko.work@yandex.ru>
2020-05-28 16:10:11 +0300

    Item almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 140) {
+            $('.header__menu').css('background-color', 'rgba(0, 0, 0, 1)');
+        }
+        else if ($(this).scrollTop() < 140) {
+            $('.header__menu').css('background-color', 'rgba(0, 0, 0, 0.5)');
+        }
+    });
+
     $(".header__menu--burger").on('click', function() {
         $(".dropdown__menu").css("transform", "translate(0)");
         $("body").css("overflow", "hidden");
@@ -9,5 +18,4 @@ $(document).ready(function() {
         $(".dropdown__menu").css("transform", "translate(-100%)");
         $("body").css("overflow", "auto");
     });
-
 });
