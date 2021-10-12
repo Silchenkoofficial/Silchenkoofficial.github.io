5c1b6030f3128eb06dcfb4e51b4e9969383620aa
silchenko <nikita.silchenko.work@yandex.ru>
2021-10-12 12:11:03 +0300

    Fix tiktok.class.js v20

--- myWorks/tiktok/assets/js/TikTok.class.js
+++ myWorks/tiktok/assets/js/TikTok.class.js
@@ -32,7 +32,7 @@ class JSTikTok {
         window.navigator.msSaveOrOpenBlob(blob, fileName);
       } else {
         // for others
-        // let url = window.URL.createObjectURL(blob);
+        let url = window.URL.createObjectURL(blob);
         const a = document.createElement("a");
         a.style.display = "none";
         a.href = url;
@@ -40,7 +40,7 @@ class JSTikTok {
         document.body.appendChild(a);
         a.click();
         a.remove();
-        // window.URL.revokeObjectURL(url);
+        window.URL.revokeObjectURL(url);
       }
     };
     xhr.send();
