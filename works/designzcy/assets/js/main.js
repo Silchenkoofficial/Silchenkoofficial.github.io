dd3b7350ce997959dec0d31ba861e3a4b40b0d42
silchenkoofficial <nikita.silchenko.work@yandex.ru>
2023-02-23 21:25:35 +0300

    Version 2.1

--- works/designzcy/assets/js/main.js
+++ works/designzcy/assets/js/main.js
@@ -1,6 +1,9 @@
 const burger = document.querySelector(".burger");
 const drawer = document.querySelector(".drawer");
 
+const copyrightElement = document.querySelector(".copyright-year");
+copyrightElement.innerHTML = new Date().getFullYear();
+
 burger.addEventListener("click", (e) => {
   burger.classList.toggle("activated");
   drawer.classList.toggle("open");
